50830bfbbd6c94ba169e3726d00ca44f374543cc
mahadev786 <68583640+mahadev786@users.noreply.github.com>
2021-06-20 20:43:20 +0530

    Create force.py

---
+++ plugins/force.py
@@ -0,0 +1,45 @@
+import logging
+logger = logging.getLogger(__name__)
+
+import os
+import datetime
+
+   from config import Config
+
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import UserNotParticipant, ChatAdminRequired, UsernameNotOccupied
+
+
+@Client.on_message(filters.private & filters.incoming)
+async def force_sub(c, m):
+    if Config.FORCE_SUB:
+        try:
+            chat = await c.get_chat_member(Config.FORCE_SUB, m.from_user.id)
+            if chat.status=='kicked':
+                return await m.reply_text('Hai you are kicked from my updates channel. So, you are not able to use me',  quote=True)
+
+        except UserNotParticipant:
+            button = [[InlineKeyboardButton('Join Our Updates Channel', url=f"https://t.me/Deva_TG_Update")]]
+            markup = InlineKeyboardMarkup(button)
+            return await m.reply_text(text="**You Must Join My Updates Channel To Use Me**", parse_mode='markdown', reply_markup=markup, quote=True)
+
+        except ChatAdminRequired:
+            logger.warning(f"Make me admin in @Deva_TG_Update")
+            if m.from_user.id in Config.AUTH_USERS:
+                return await m.reply_text(f"Make me admin in @Deva_TG_Update")
+
+        except UsernameNotOccupied:
+            logger.warning("The forcesub username was Incorrect. Please give the correct username.")
+            if m.from_user.id in Config.AUTH_USERS:
+                return await m.reply_text("The forcesub username was Incorrect. Please give the correct username.")
+
+        except Exception as e:
+            if "belongs to a user" in str(e):
+                logger.warning("Forcesub username must be a channel username Not yours or any other users username")
+                if m.from_user.id in Config.AUTH_USERS:
+                    return await m.reply_text("Forcesub username must be a channel username Not yours or any other users username")
+            logger.error(e)
+            return await m.reply_text("Some thing went wrong. Try again and if same issue occur contact [our Channel](https://t.me/Deva_TG_Update)", disable_web_page_preview=True, quote=True)
+
+    await m.continue_propagation()
